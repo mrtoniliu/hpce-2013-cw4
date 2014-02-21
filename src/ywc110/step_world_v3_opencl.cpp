bdf9d717e4779dc52135686f9d91e0ea54dac695
Yong Wen Chua <lawliet89@gmail.com>
2014-02-21 11:31:02 +0000

    Put output back

--- src/ywc110/step_world_v3_opencl.cpp
+++ src/ywc110/step_world_v3_opencl.cpp
@@ -93,11 +93,11 @@ void StepWorldV3OpenCL(world_t &world, float dt, unsigned n)
 	if(platforms.size()==0)
 	    throw std::runtime_error("No OpenCL platforms found.");
 
-	// std::cerr<<"Found "<<platforms.size()<<" platforms\n";
-	// for(unsigned i=0;i<platforms.size();i++){
-	//     std::string vendor=platforms[0].getInfo<CL_PLATFORM_VENDOR>();
-	//     std::cerr<<"  Platform "<<i<<" : "<<vendor<<"\n";
-	// }
+	std::cerr<<"Found "<<platforms.size()<<" platforms\n";
+	for(unsigned i=0;i<platforms.size();i++){
+	    std::string vendor=platforms[0].getInfo<CL_PLATFORM_VENDOR>();
+	    std::cerr<<"  Platform "<<i<<" : "<<vendor<<"\n";
+	}
 
 	int selectedPlatform=0;
 	if(getenv("HPCE_SELECT_PLATFORM")){
@@ -112,11 +112,11 @@ void StepWorldV3OpenCL(world_t &world, float dt, unsigned n)
 	    throw std::runtime_error("No opencl devices found.\n");
 	}
 
-	// std::cerr<<"Found "<<devices.size()<<" devices\n";
-	// for(unsigned i=0;i<devices.size();i++){
-	//     std::string name=devices[i].getInfo<CL_DEVICE_NAME>();
-	//     std::cerr<<"  Device "<<i<<" : "<<name<<"\n";
-	// }
+	std::cerr<<"Found "<<devices.size()<<" devices\n";
+	for(unsigned i=0;i<devices.size();i++){
+	    std::string name=devices[i].getInfo<CL_DEVICE_NAME>();
+	    std::cerr<<"  Device "<<i<<" : "<<name<<"\n";
+	}
 
 	int selectedDevice=0;
 	if(getenv("HPCE_SELECT_DEVICE")){
